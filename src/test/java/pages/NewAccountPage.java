6bc2c466aceafd1869add17cc4a39237d12cd9d9
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-05-17 00:51:29 +0300

    Added NewAccountPage Added locators for NewAccountPage

--- src/test/java/pages/NewAccountPage.java
+++ src/test/java/pages/NewAccountPage.java
@@ -4,8 +4,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NewAccountPage extends BasePage {
-    public static final By PHONE_INPUT = By.xpath("//*[@data-aura-class='forceDetailPanelDesktop']//span[text()='Phone']following-sibling::input");
+    String INPUT_FIELD = "//*[@data-aura-class='forceDetailPanelDesktop']//span[text()='%s']//ancestor::label//following-sibling::input";
+    String phone = "Phone";
+    /* Fax
+    Website
+    Employees
+    Annual Revenue
+    Billing City
+    Billing Zip/Postal Code
+    Billing Country
+    Shipping Address
+    Shipping Street
+    Shipping City
+    Shipping State/Province
+    Shipping Zip/Postal Code
+    Shipping Country
+     */
 
+    String SEARCH_FIELD = "//*[@data-aura-class='forceDetailPanelDesktop']//span[text()='%s']//ancestor::label//following-sibling::div//child::input";
+    /*
+    Account Name
+    Parent Account
+    */
+
+    String TEXTAREA_FIELD = "//*[@data-aura-class='forceDetailPanelDesktop']//span[text()='%s']//ancestor::label//following-sibling::textarea";
+    /*
+    Description
+    Shipping Street
+     */
+    String BILLING_TO_SHIPPING_CHECKBOX = "//*[@data-aura-class='forceDetailPanelDesktop']//span[text()='Copy Billing Address to Shipping Address']//ancestor::label//following-sibling::input"; // the same as INPUT_FIELD
+    String FOOTER_BUTTONS = "//*[@class='inlineFooter']//span[text()='%s']//ancestor::button";
+    /*
+    Save
+    Save & New
+    Cancel
+     */
 
     public NewAccountPage(WebDriver driver) {
         super(driver);
@@ -13,6 +46,6 @@ public class NewAccountPage extends BasePage {
 
     @Override
     public boolean isPageOpen() {
-        return isExist(PHONE_INPUT);
+        return isExist(By.xpath(String.format(INPUT_FIELD, phone)));
     }
 }
