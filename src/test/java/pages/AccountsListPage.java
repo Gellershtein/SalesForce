7cb44fcb765054376352f9018fbcbfa0255241f7
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-05-17 01:23:11 +0300

    Added NewAccountPage Added locators for NewAccountPage

--- src/test/java/pages/AccountsListPage.java
+++ src/test/java/pages/AccountsListPage.java
@@ -6,6 +6,14 @@ import org.openqa.selenium.WebDriver;
 public class AccountsListPage extends BasePage {
     public static final By NEW_BUTTON = By.xpath("//*[@title='New']");
     By icon = By.cssSelector("img[title='Contacts']");
+    String SHOW_MORE_ACTIONS_BUTTON = "//span[text()='Show more actions']//ancestor::a";
+    String ACTIONS_BUTTONS = "//*[@class='scrollable']//child::a[@title='%s']"; //after clicking SHOW_MORE_ACTIONS_BUTTON and wait for rendering
+    /*
+    Edit
+    Delete
+    Change Owner
+     */
+    String SEARCH_FIELD = "//*[@placeholder='Search this list...']";
 
     public AccountsListPage(WebDriver driver) {
         super(driver);
